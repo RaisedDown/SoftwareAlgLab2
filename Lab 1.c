0ab4a62a732cb2b019506d86b0c3a329694df007
RaisedDown <tuj56890@temple.edu>
2023-09-04 17:30:42 -0400

    what

---
+++ Lab 1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>;
+#include <stdlib.h>;
+
+int main(){
+    int getDays;
+    char getDecision;
+    while (getDecision != 'N'){
+        printf("New D Value? Y/N");
+        scanf("%c", getDecision);
+        if(getDecision == 'Y'){          
+        printf("Enter the number of days or exit: ");
+        scanf("%d", &getDays);
+            double newInfection;
+        }
+    }
+
+
+
+
+    return 0;
+}
+
+
+
+double newInfection(int getDays){
+    double noInfections = 7, repoRate = 1.2;
+    int pop = 39470;
+    double getInfections;
+    getInfections = repoRate * getDays - 1;
+    int totalInfections;
+    for(int isDay = 0; isDay < getDays; isDay++){
+        totalInfections += getInfections;
+    }
+
+return getInfections, totalInfections;
+}
+
+int Refundcost(int infections){
+
+}
